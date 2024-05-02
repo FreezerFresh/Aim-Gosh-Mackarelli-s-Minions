e32ccda1bb7a2c1bcd749cc305a0822390c3d0af
orozcoborboa_0959458 <orozcoborboa_0959458>
2024-05-01 23:25:13 -0700

    added client.cc

---
+++ client.cc
@@ -0,0 +1,17 @@
+#include <cstdlib>
+#include<iostream>
+#include<string>
+#include<boost/asio.hpp>
+using namespace std;
+using boost::asio::ip::tcp;
+
+int main(int argc, char* argv[]){
+	try {
+		
+	}
+	catch (const std::exception&) {
+	}
+	return 0;
+}
+
+
