02089420a3f76e1d203cc2b44ac0983a8e28b8ab
cardenasisael1 <156411115+cardenasisael1@users.noreply.github.com>
2024-05-04 22:59:56 -0700

    Update thread.cc

--- thread.cc
+++ thread.cc
@@ -1,1 +1,11 @@
 //this is thread.cc
+#include <iostream>
+#include <thread>
+using namespace std;
+
+void thread(string test) {
+  cout << Persona << ", this is the first test" << endl;
+  this_thread::sleep_for(chrono::seconds(2));
+  cout << Persona << ", this is the second test" << endl;
+
+}
