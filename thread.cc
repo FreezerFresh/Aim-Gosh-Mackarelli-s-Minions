68422502fdc9874afb24f115195dc19e774faaf1
cardenasisael1 <156411115+cardenasisael1@users.noreply.github.com>
2024-05-04 23:14:30 -0700

    Update thread.cc

--- thread.cc
+++ thread.cc
@@ -1,11 +1,15 @@
 //this is thread.cc
 #include <iostream>
 #include <thread>
+#include <mutex>
 using namespace std;
 
+mutex restriction;
 void thread(string test) {
+  unique_lock<mutex> locked(restriction);
   cout << Persona << ", this is the first test" << endl;
   this_thread::sleep_for(chrono::seconds(2));
   cout << Persona << ", this is the second test" << endl;
-
+  locked.unlock();
 }
+
