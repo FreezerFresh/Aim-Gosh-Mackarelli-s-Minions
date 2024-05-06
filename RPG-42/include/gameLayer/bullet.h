f52f0d539f28fa77791391edb03d449794768410
Joshua Garza <157847511+BushyJ@users.noreply.github.com>
2024-05-06 06:34:56 -0700

    Add files via upload

---
+++ RPG-42/include/gameLayer/bullet.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <gl2d/gl2d.h>
+
+struct Bullet {
+	glm::vec2 position = {};
+	glm::vec2 fireDir = {};
+
+	void render(gl2d::Renderer2D &renderer, gl2d::Texture bulletTex, gl2d::TextureAtlasPadding bulletsAtlas);
+	void update(float deltaTime);
+};
